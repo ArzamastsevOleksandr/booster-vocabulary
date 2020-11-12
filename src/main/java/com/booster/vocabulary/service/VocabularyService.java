da19649dc7bf1a4a5d7fd1e189d7b023b9fa190e
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2020-11-13 01:52:47 +0200

    refactor: delete redundant user search

--- src/main/java/com/booster/vocabulary/service/VocabularyService.java
+++ src/main/java/com/booster/vocabulary/service/VocabularyService.java
@@ -3,9 +3,7 @@ package com.booster.vocabulary.service;
 import com.booster.vocabulary.dto.VocabularyDto;
 import com.booster.vocabulary.dto.VocabularyEntryDto;
 import com.booster.vocabulary.entity.VocabularyEntity;
-import com.booster.vocabulary.exception.UserEntityByIdNotFoundException;
 import com.booster.vocabulary.exception.VocabularyEntityByIdNotFoundException;
-import com.booster.vocabulary.repository.UserRepository;
 import com.booster.vocabulary.repository.VocabularyEntryRepository;
 import com.booster.vocabulary.repository.VocabularyRepository;
 import lombok.RequiredArgsConstructor;
@@ -23,12 +21,8 @@ public class VocabularyService {
 
     private final VocabularyRepository vocabularyRepository;
     private final VocabularyEntryRepository vocabularyEntryRepository;
-    private final UserRepository userRepository;
 
     public List<VocabularyDto> findAllForUserId(Long userId) {
-        userRepository.findById(userId)
-                .orElseThrow(() -> new UserEntityByIdNotFoundException(userId));
-
         List<VocabularyEntity> vocabularyEntityList = vocabularyRepository.findByUserId(userId);
 
         return vocabularyEntityList.stream()
